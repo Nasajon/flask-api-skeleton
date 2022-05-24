92189376ce89623e131c2794a5e9577c02510273
Carlos Benevides <carlosbenevides@nasajon.com.br>
2022-05-23 23:03:29 -0300

    Suporte para conexao remota

--- nasajon/wsgi.py
+++ nasajon/wsgi.py
@@ -1,10 +1,17 @@
 # Importando arquivos de configuração
+import os
+
+# import debugpy
 from nasajon.settings import application
 import nasajon.db_pool_config
 
 # TODO Importar todos os controllers (se não, as rotas não existirão)
 import nasajon.controller.clientes_controller
 import nasajon.controller.token_info_controller
-
+import ptvsd
+ptvsd.enable_attach(("0.0.0.0",9000))
+# debugpy.configure(python="/usr/bin/python3")
+# debugpy.listen(("0.0.0.0",9000))
 if __name__ == '__main__':
-    application.run(port=5000)
+    application.run(port=5000,debug=True)
+    
