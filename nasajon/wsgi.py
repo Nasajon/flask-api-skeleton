6478e37ac96f3395f5152d47fddc5473f228dfe6
Sergio Silva <sergiosilva@nasajon.com.br>
2023-01-05 19:32:01 -0300

    Refatorando o healthcheck para biblioteca

--- nasajon/wsgi.py
+++ nasajon/wsgi.py
@@ -1,7 +1,14 @@
 # Importando arquivos de configuração
 from nasajon.settings import application
 import nasajon.db_pool_config
-import nasajon.healthcheck_config
+from nasajon.injector_factory import InjectorFactory
+
+# Configurando o healthcheck
+from nsj_rest_lib.healthcheck_config import HealthCheckConfig
+HealthCheckConfig(
+    flask_application=application,
+    injector_factory_class=InjectorFactory
+).config(True, True)
 
 # TODO Importar todos os controllers (se não, as rotas não existirão)
 import nasajon.controller.cliente_controller
